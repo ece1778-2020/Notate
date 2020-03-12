1820d79e08f0e322012bfe8ccfa4bf7c8ea4a835
zzhdsb123 <zzhdsb123@gmail.com>
2020-03-12 17:51:32 -0400

    delete files

--- Notate/Notate/ContentView.swift
+++ Notate/Notate/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        MainMenu()
-//        SheetView()
+//        MainMenu()
+        TimerView(timerhelper: TimerHelper())
     }
 }
 
