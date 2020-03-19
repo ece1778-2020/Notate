817545dac581140f1426eb1cdc15bcaa5cea2895
zzhdsb123 <zzhdsb123@gmail.com>
2020-03-18 22:23:05 -0400

    scores implemented

--- Notate/Notate/Views/LyricsView.swift
+++ Notate/Notate/Views/LyricsView.swift
@@ -5,7 +5,7 @@
 //  Created by Artorias on 2020-03-02.
 //  Copyright © 2020 Yilun Huang. All rights reserved.
 //
-
+import Foundation
 import SwiftUI
 
 struct LyricsView: View {
@@ -13,6 +13,7 @@ struct LyricsView: View {
     @State var filename: String = ""
     @State var show_alert = false
     @State var alert_message = ""
+    @State var show_sheet = false
     @EnvironmentObject var obj: Observed
     @Binding var navigationBarIsHidden: Bool
     
@@ -22,12 +23,16 @@ struct LyricsView: View {
     }
     
     func saveFile() {
-        if self.filename == "" {
+        if self.filename == "" || self.filename == "files" {
             self.alert_message = "Invalid filename!"
             self.show_alert.toggle()
         }
         else if self.text == "" {
             self.alert_message = "Empty lyrics!"
+            self.show_alert.toggle()
+        }
+        else if self.filename.contains(" ") {
+            self.alert_message = "No empty space allowed in file name!"
             self.show_alert.toggle()
         }
         else {
@@ -36,8 +41,7 @@ struct LyricsView: View {
             let str = self.text
             do {
                 try str.write(to: url, atomically: true, encoding: .utf8)
-                self.alert_message = "Success!"
-                self.show_alert.toggle()
+                self.add_file(name: self.filename)
             } catch {
                 self.alert_message = error.localizedDescription
                 self.show_alert.toggle()
@@ -45,19 +49,62 @@ struct LyricsView: View {
         }
     }
     
-    func printFile () {
+    func add_file (name: String) {
+        let documentPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let filePath = documentPath.appendingPathComponent("files.txt")
+        do {
+            var file_list = try String(contentsOf: filePath, encoding: .utf8)
+            var files = file_list.components(separatedBy: " ")
+            if !files.contains(name) {
+                files.append(name)
+            }
+            file_list = files.joined(separator: " ")
+            let url = documentPath.appendingPathComponent("files.txt")
+            do {
+                try file_list.write(to: url, atomically: true, encoding: .utf8)
+                self.alert_message = "Success!"
+                self.show_alert.toggle()
+            }
+            catch {
+                self.alert_message = error.localizedDescription
+                self.show_alert.toggle()
+            }
+            
+        }
+        catch {
+            self.alert_message = error.localizedDescription
+            self.show_alert.toggle()
+        }
+        
+    }
+    
+    func test () {
+        let inputString = "This123Is123A123Test"
+        let splits = inputString.components(separatedBy: "123")
+        print(splits)
+    }
+    
+    func check_list () {
         let documentPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
         // filename here
         
-        let filePath = documentPath.appendingPathComponent("lyrics.txt")
+        let filePath = documentPath.appendingPathComponent("files.txt")
         do {
             let content = try String(contentsOf: filePath, encoding: .utf8)
             print(content)
         }
         catch {
-            print("Error")
+            let content = ""
+            let url = documentPath.appendingPathComponent("files.txt")
+            do {
+                try content.write(to: url, atomically: true, encoding: .utf8)
+            }
+            catch {
+                self.alert_message = error.localizedDescription
+                self.show_alert.toggle()
+            }
+            
         }
-        
     }
     
     var body: some View {
@@ -97,12 +144,30 @@ struct LyricsView: View {
                 }
             }
 //            Button(action: {
-//                self.printFile()
+//
 //            }) {
 //                Text("test")
 //            }
+//                .font(.subheadline)
+//                .foregroundColor(Color.white)
+//                .padding()
+//                .background(Color(red: 100 / 255, green: 100 / 255, blue: 100 / 255))
+//                .padding()
+//                .shadow(radius: 10)
         }
+            .sheet(isPresented: self.$show_sheet, content: {
+                ScoresView(lyrics: self.$text, file_name: self.$filename, show_sheet: self.$show_sheet)
+            })
             .navigationBarTitle("Lyrics", displayMode: .inline)
+            .navigationBarItems(trailing: HStack {
+                Button(action: {
+                    self.show_sheet.toggle()
+                }) {
+                    Image(systemName: "doc.text.fill")
+                        .foregroundColor(.white)
+                }
+                
+            })
             
             .alert(isPresented: self.$show_alert) {
                 Alert(title: Text(self.alert_message))
@@ -118,6 +183,7 @@ struct LyricsView: View {
         
             .onAppear() {
                 self.navigationBarIsHidden = false
+                self.check_list()
             }
         
     }
---
+++ Notate/Notate/Views/ScoresView.swift
@@ -0,0 +1,70 @@
+//
+//  ScoresView.swift
+//  Notate
+//
+//  Created by Artorias on 2020-03-18.
+//  Copyright © 2020 Yilun Huang. All rights reserved.
+//
+
+import SwiftUI
+import Foundation
+
+struct ScoresView: View {
+    @Binding var lyrics: String
+    @Binding var file_name: String
+    @Binding var show_sheet: Bool
+    @State var file_list = [String]()
+    @EnvironmentObject var observed: Observed
+    
+    func loadFileList () {
+        let documentPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let filePath = documentPath.appendingPathComponent("files.txt")
+        do {
+            let shit = try String(contentsOf: filePath, encoding: .utf8)
+            let temp = shit.components(separatedBy: " ")
+            self.file_list = Array(temp[1...])
+            print(self.file_list)
+        }
+        catch {
+            print("Error!")
+        }
+    }
+    
+    func getContent (name: String) {
+        let documentPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+        let filePath = documentPath.appendingPathComponent("\(name).txt")
+        do {
+            let content = try String(contentsOf: filePath, encoding: .utf8)
+            self.lyrics = content
+            self.file_name = name
+            self.show_sheet.toggle()
+        }
+        catch {
+            print("Error!")
+        }
+    }
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(self.file_list, id: \.self) { file in
+                    Button(action: {
+                        self.getContent(name: file)
+                    }) {
+                        Text(file)
+                    }
+                }
+            }
+        }
+        .onAppear() {
+            self.loadFileList()
+        }
+    }
+    
+}
+
+//struct ScoresView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ScoresView()
+//    }
+//}
